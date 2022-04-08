763e7c62179cad950803cf72caaaf9dc6b248f13
Gwyn <Gwyn@helloworld.com>
2022-04-08 22:12:56 +0800

    second commit

--- src/main/java/com/sh/git/GitTest.java
+++ src/main/java/com/sh/git/GitTest.java
@@ -3,6 +3,7 @@ package com.sh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
 
     }
 }
