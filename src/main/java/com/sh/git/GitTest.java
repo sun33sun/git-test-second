8b41944596188a9738a9ffbb632870b4cb1c4e62
Gwyn <Gwyn@helloworld.com>
2022-04-08 23:14:39 +0800

    test

--- src/main/java/com/sh/git/GitTest.java
+++ src/main/java/com/sh/git/GitTest.java
@@ -2,12 +2,6 @@ package com.sh.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git3!");
-        System.out.println("hello git4!");
-        System.out.println("hot-fix git!");
-        System.out.println("master git!");
         System.out.println("push git!");
     }
 }
