62bf227c1a33689fe5ceea2c9247f68c4d67447e
sun33sun <82629209+sun33sun@users.noreply.github.com>
2022-04-09 15:17:20 +0800

    Update GitTest.java

--- src/main/java/com/sh/git/GitTest.java
+++ src/main/java/com/sh/git/GitTest.java
@@ -2,6 +2,7 @@ package com.sh.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("push git!");
+        System.out.println("pull test!");
+        System.out.println("pull testt!");
     }
 }
